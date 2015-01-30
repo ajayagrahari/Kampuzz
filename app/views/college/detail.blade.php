607d77d2ae727000a02996674d6ffb4a00159212
ajayagrahari <ajaykumar.gupta@netfunda.com>
2015-01-30 16:43:06 +0530

    Update detail.blade.php

--- app/views/college/detail.blade.php
+++ app/views/college/detail.blade.php
@@ -150,4 +150,10 @@
            
         </div>
 
+<script>
+$(function(){
+	$('.fadein img:gt(0)').hide();
+	setInterval(function(){$('.fadein :first-child').fadeOut().next('img').fadeIn().end().appendTo('.fadein');}, 3000);
+});
+</script>
         @stop
